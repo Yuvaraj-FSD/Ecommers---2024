67d93b2d955f3b179f73145da8029ef1e89c6c67
YUVARAJ D <122588778+Yuvaraj-FSD@users.noreply.github.com>
2026-04-07 21:04:22 +0530

    Delete carousel.js

--- carousel.js
+++
@@ -1,8 +0,0 @@
-// Optional: Pause animation on hover
-const track = document.getElementById('brand-track');
-track.addEventListener('mouseenter', () => {
-  track.style.animationPlayState = 'paused';
-});
-track.addEventListener('mouseleave', () => {
-  track.style.animationPlayState = 'running';
-});
